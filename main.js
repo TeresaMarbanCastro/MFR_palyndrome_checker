737977c9d40c7bf32f4a585576013ae127c9a9de
Teresa <teresamarbancastro@gmail.com>
2021-06-02 20:06:34 +0200

    include index, main and basic styles for exercise

---
+++ main.js
@@ -0,0 +1,10 @@
+const result = document.querySelector('.result-container');
+const button = document.querySelector('.check-btn');
+
+
+function checkPalyndrome() {
+    const inputValue = document.querySelector('input').value;
+
+}
+
+button.addEventListener('click', checkPalyndrome);
